f5badafa61350be01bac14810d902f4ff9f98b84
Noah Padgett <80340752+redred623@users.noreply.github.com>
2021-09-03 11:48:53 -0400

    Removed enterprise specific data

--- Get_AD_group_members.ps1
+++ Get_AD_group_members.ps1
@@ -2,9 +2,9 @@
 #note: this will include all members of all groups nested into this group. If you only want the direct members in the group remove '-recursive' from get-adgroupmember 
 
 #type out the file path here between the single quotes
-$path = '\\noahpadgel\c$\test.csv'
+$path = '\\*computername*\c$\test.csv'
 
 #find the group name in AD and put it here between single quotes
-$name= 'zss'
+$name= '**'
 
 Get-ADGroupMember $name -Recursive | foreach-object {get-aduser $_.samaccountname -properties emailaddress,employeenumber,department,title| select name,samaccountname,emailaddress,employeenumber,department,title} |  Export-Csv -NoTypeInformation -Path $path
