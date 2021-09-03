993054081482ce5c8312b26869d87314efe968b6
Noah Padgett <80340752+redred623@users.noreply.github.com>
2021-09-03 13:16:56 -0400

    Removed enterprise specific data

--- Useful_commands.ps1
+++ Useful_commands.ps1
@@ -6,7 +6,7 @@ get-aduser -filter * -Properties employeenumber | Where-Object {$_.employeenumbe
 
 #allows you check last boot time for a computer remotely
 
-Get-CimInstance -ClassName win32_operatingsystem -ComputerName Annaboltol2 | select csname, lastbootuptime
+Get-CimInstance -ClassName win32_operatingsystem -ComputerName *computer name* | select csname, lastbootuptime
 
 #allows you to get the members of a distribution group. Need to create a variable called members. 
 #Members needs to be a list of emails seperated by commas 
@@ -18,16 +18,15 @@ $members = @(
 $members | foreach-object{
     >> $name = $_
     >> Get-DistributionGroupMember $name | select name, @{name = 'group'; expression = {$name}}
-    >> } | Export-Csv -Path '\\noahpadgel\c$\code library\export_raj.csv' -NoTypeInformation
+    >> } | Export-Csv -Path '*csv file path*' -NoTypeInformation
 
 #allows you to create a standard distribution group 
 
-New-DistributionGroup -name 'DL-DIV-SouthEast-Georgia-Tyrone-TyroneQuarryFAManagement'`
- -alias 'TyroneQuarryFAManagement' -ManagedBy Luke.Phelps@martinmarietta.com `
- -Members Jason.Watson@martinmarietta.com,Troy.Aikens@martinmarietta.com,Isaac.Clizbe@martinmarietta.com,`
- Christopher.Leonard@martinmarietta.com -MemberJoinRestriction Closed `
- -Notes 'This will be to send automatic reporting to the management team at Tyrone Quarry. 
- Managed by Luke Phelps'
+New-DistributionGroup -name '*name of group'`
+ -alias '*email address alias*' -ManagedBy *insert email* `
+ -Members *insert members* -MemberJoinRestriction Closed `
+ -Notes 'This will be to send automatic reporting to the management team at *location*. 
+ Managed by *insert name*'
 
 <#
 name is the name you can search for in shell and AD, 
@@ -41,7 +40,7 @@ notes, adds to the description of the group
 
 #Find all members of an AD group. 
 
-Get-ADGroupMember "All App Users-JDE9 PO Originators" -Recursive | foreach-object {get-aduser $_.samaccountname -properties emailaddress,employeenumber,department,description| select name,samaccountname,emailaddress,employeenumber,department,description} |  Export-Csv -NoTypeInformation -Path '\\noahpadgel\c$\code library\Brendan_export.csv'
+Get-ADGroupMember "**insert name**" -Recursive | foreach-object {get-aduser $_.samaccountname -properties emailaddress,employeenumber,department,description| select name,samaccountname,emailaddress,employeenumber,department,description} |  Export-Csv -NoTypeInformation -Path '**insert file path**'
 
 #find out what distribution groups a user is apart of of. Takes a little while to run. 
 
@@ -51,7 +50,7 @@ $DistributionGroups = Get-DistributionGroup -resultsize unlimited | where { (Get
 
 #create ad security group with global scope from a csv with columns 'name' and 'description' 
 
-Import-Csv -Path '\\noahpadgel\c$\Code Library\chester_import.csv' | foreach {New-ADGroup -Name $_.name  -GroupCategory Security -GroupScope Global -Path "OU=EnterpriseGroups,DC=martinmarietta,DC=com" -Description $_.description}
+Import-Csv -Path '**insert file path**' | foreach {New-ADGroup -Name $_.name  -GroupCategory Security -GroupScope Global -Path "**create OU path**" -Description $_.description}
 
 #find a user based on first name and last name
 
